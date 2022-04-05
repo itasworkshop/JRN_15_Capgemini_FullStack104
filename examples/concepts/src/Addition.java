df56079ec8b052eacdc76be9ec304cd4ea8c9f04
itasworkshop <itasworkshop@gmail.com>
2022-04-05 11:58:43 +0530

    05 April 2

---
+++ examples/concepts/src/Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+
+    public static void main(String[] args){
+       int a = 10; //a is variable --> placeholder
+       int b = 20;
+
+       int result = a+b;
+       //System.out.println(result);
+        System.out.println("Result of a and b is "+ result);
+    }
+}
